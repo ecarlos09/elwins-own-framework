aa77d2cd0c99429f575992d2ff5fd4716b63528d
Elwin <elwin.carlos09@gmail.com>
2021-03-07 21:42:05 +0000

    Finish editing step 4 of README and execute

--- server/app.js
+++ server/app.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my server!');
+});
+
+app.listen(port, () => console.log('Visit http://localhost:${port} to see our content!'));

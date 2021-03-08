a50ae44534e42c5c9dcac79de148db17da22c893
Elwin <elwin.carlos09@gmail.com>
2021-03-08 16:29:59 +0000

    add client side files

---
+++ client/js/index.js
@@ -0,0 +1,15 @@
+//Setup event listeners here
+
+  
+// ********************************************
+// SETUP
+const btn = document.querySelector('button');
+const form = document.querySelector('#new-cat-form');
+
+// Bind event listeners
+btn.addEventListener('click', getMessage);
+form.addEventListener('submit', submitCat);
+
+// Fetch all cats as soon as app is loaded
+getAllCats();
+// ********************************************
